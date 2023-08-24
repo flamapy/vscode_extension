ab614dfe2881cdc6139c7149a8923f2623e5cd30
José A. Galindo <jagalindo@us.es>
2023-08-24 17:29:25 +0200

    fix: adding all operations and some optimizations

--- extension.js
+++ extension.js
@@ -18,6 +18,10 @@ import('node-fetch').then(fetchModule => {
 let globalPyodideInstance = null;
 let packagesLoaded = false;
 
+let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+        statusBarItem.text = "$(sync~spin) Processing...";
+
+
 async function getPyodideInstance() {
     if (!globalPyodideInstance) {
         globalPyodideInstance = await loadPyodide();
@@ -73,106 +77,188 @@ async function exec_python(code) {
     pyodide.FS.writeFile("uvlfile.uvl", uvl_file, { encoding: "utf8" });
    
     return pyodide.runPythonAsync(
-        code
+        `
+        from flamapy.interfaces.python.FLAMAFeatureModel import FLAMAFeatureModel
+        fm = FLAMAFeatureModel("uvlfile.uvl")
+        `+code
     );
 }
 
+class ButtonProvider {
+    getTreeItem(element) {
+        return element;
+    }
+
+    getChildren(element) {
+        if (!element) {
+            return [
+                this.createButton('Products', 'flamapy.products'),
+                this.createButton('Valid', 'flamapy.valid'),
+                this.createButton('Atomic Sets', 'flamapy.atomic_sets'),
+                this.createButton('Avg Branching Factor', 'flamapy.average_branching_factor'),
+                this.createButton('Count Leafs', 'flamapy.count_leafs'),
+                this.createButton('Estimated #Configurations', 'flamapy.estimated_number_of_products'),
+                this.createButton('Leaf feature lists', 'flamapy.leaf_features'),
+                this.createButton('Max Depth', 'flamapy.max_depth'),
+                this.createButton('Core Features', 'flamapy.core_features'),
+                this.createButton('Dead Features', 'flamapy.dead_features'),
+                this.createButton('Error detection', 'flamapy.error_detection'),
+                this.createButton('False Optional Features', 'flamapy.false_optional_features'),
+                this.createButton('#Configurations', 'flamapy.products_number')
+            ];
+        }
+        return [];
+    }
+
+    createButton(label, command) {
+        const treeItem = new vscode.TreeItem(label, vscode.TreeItemCollapsibleState.None);
+        treeItem.command = { 
+            command: command,
+            title: label,
+            arguments: [] 
+        };
+        return treeItem;
+    }
+}
+
+function createCommand(id,code,result_header){
+    let command = vscode.commands.registerCommand(id, function () {
+        
+        statusBarItem.show();
+        
+        exec_python(code).then((result) => {
+            const panel = vscode.window.createWebviewPanel(
+                'resultDisplay', 
+                result_header, 
+                vscode.ViewColumn.Two, 
+                {}
+            );
+            panel.webview.html = `<html><body>`+result+`</body></html>`;
+            statusBarItem.hide()
+        });
+    });
+    return command
+}
 function activate(context) {
-    let products = vscode.commands.registerCommand('flamapy.products', function () {
-        exec_python(
+    const provider = new ButtonProvider();
+    vscode.window.registerTreeDataProvider('yourExtensionView', provider);
+  
+    let flamapy_products = createCommand(
+        'flamapy.products',
         `
-        from flamapy.interfaces.python.FLAMAFeatureModel import FLAMAFeatureModel
-    
-        fm = FLAMAFeatureModel("uvlfile.uvl")
+        
         result=fm.products()
         "<br>".join([f'P({i}): {p}' for i, p in enumerate(result, 1)])    
-        `        
-        ).then((result) => {
-            const panel = vscode.window.createWebviewPanel(
-                'resultDisplay', 
-                'Products Result', 
-                vscode.ViewColumn.Two, 
-                {}
-            );
-            
-            panel.webview.html = `<html><body>`+result+`</body></html>`;
-        });
-    });
+        `,
+        'Products Result')
 
-    context.subscriptions.push(products);
-
-
-    let valid = vscode.commands.registerCommand('flamapy.valid', function () {
-        exec_python(
+    let flamapy_valid = createCommand(
+        'flamapy.valid',
         `
-        from flamapy.interfaces.python.FLAMAFeatureModel import FLAMAFeatureModel
-    
-        fm = FLAMAFeatureModel("uvlfile.uvl")
         result=fm.valid()
         "The feature model is valid? " + str(result)
-        `        
-        ).then((result) => {
-            const panel = vscode.window.createWebviewPanel(
-                'resultDisplay', 
-                'Valid Result', 
-                vscode.ViewColumn.Two, 
-                {}
-            );
-            
-            panel.webview.html = `<html><body>`+result+`</body></html>`;
-        });
-        
-    });
+        `,
+        'Valid Result')
 
-    context.subscriptions.push(valid);
-
-    let dead_features = vscode.commands.registerCommand('flamapy.dead_features', function () {
-        exec_python(
+    let flamapy_dead_features = createCommand(
+        'flamapy.dead_features',
         `
-        from flamapy.interfaces.python.FLAMAFeatureModel import FLAMAFeatureModel
-    
-        fm = FLAMAFeatureModel("uvlfile.uvl")
         result=fm.dead_features()
         "<br>".join([f'F({i}): {p}' for i, p in enumerate(result, 1)])    
-        `        
-        ).then((result) => {
-            const panel = vscode.window.createWebviewPanel(
-                'resultDisplay', 
-                'Dead Features Result', 
-                vscode.ViewColumn.Two, 
-                {}
-            );
-            
-            panel.webview.html = `<html><body>`+result+`</body></html>`;
-        });
-        
-    });
+        ` ,
+        'Dead Features Result')
 
-    context.subscriptions.push(dead_features);
-
-    let false_optional_features = vscode.commands.registerCommand('flamapy.false_optional_features', function () {
-        exec_python(
+    let flamapy_false_optional_features = createCommand(
+        'flamapy.false_optional_features',
         `
-        from flamapy.interfaces.python.FLAMAFeatureModel import FLAMAFeatureModel
-    
-        fm = FLAMAFeatureModel("uvlfile.uvl")
         result=fm.false_optional_features()
         "<br>".join([f'F({i}): {p}' for i, p in enumerate(result, 1)])    
-        `        
-        ).then((result) => {
-            const panel = vscode.window.createWebviewPanel(
-                'resultDisplay', 
-                'False Optional Features Result', 
-                vscode.ViewColumn.Two, 
-                {}
-            );
-            
-            panel.webview.html = `<html><body>`+result+`</body></html>`;
-        });
+        `,
+        'False Optional Features Result')
+    
+    let flamapy_core_features = createCommand(
+        'flamapy.core_features',
+        `
+        result=fm.core_features()
+        "<br>".join([f'F({i}): {p}' for i, p in enumerate(result, 1)])    
+        `,
+        'Core Features Result')
+    
+    let flamapy_atomic_sets = createCommand(
+        'flamapy.atomic_sets',
+        `
+        result=fm.atomic_sets()
+        "<br>".join([f'AS({i}): {p}' for i, p in enumerate(result, 1)])    
+        `,
+        'Atomix Sets Result')
         
-    });
+    let flamapy_average_branching_factor = createCommand(
+        'flamapy.average_branching_factor',
+        `
+        result=fm.average_branching_factor()
+        "<br>"+str(result)
+        `,
+        'Avg Branching Factor Result')
 
-    context.subscriptions.push(false_optional_features);
+    let flamapy_count_leafs = createCommand(
+        'flamapy.count_leafs',
+        `
+        result=fm.count_leafs()
+        "<br>"+str(result)
+        `,
+        'Avg Branching Factor Result')
+
+    let flamapy_estimated_number_of_products = createCommand(
+        'flamapy.estimated_number_of_products',
+        `
+        result=fm.estimated_number_of_products()
+        "<br>"+str(result)
+        `,
+        'Estimated #Configurations Result')        
+
+    let flamapy_leaf_features = createCommand(
+        'flamapy.leaf_features',
+        `
+        result=fm.leaf_features()
+        "<br>".join([f'AS({i}): {p}' for i, p in enumerate(result, 1)])    
+        `,
+        'Leaf Features Result')        
+    let flamapy_max_depth = createCommand(
+        'flamapy.max_depth',
+        `
+        result=fm.max_depth()
+        "<br>"+str(result)
+        `,
+        'Max Depth Result')     
+    let flamapy_error_detection = createCommand(
+        'flamapy.error_detection',
+        `
+        result=fm.max_depth()
+        "<br>"+str(result)
+        `,
+        'Errors Result') 
+
+    let flamapy_products_number = createCommand(
+        'flamapy.products_number',
+        `
+        result=fm.products_number()
+        "<br>"+str(result)
+        `,
+        '#Configurations Result')  
+
+    context.subscriptions.push(flamapy_products)
+    context.subscriptions.push(flamapy_valid)
+    context.subscriptions.push(flamapy_atomic_sets)
+    context.subscriptions.push(flamapy_average_branching_factor)
+    context.subscriptions.push(flamapy_count_leafs)
+    context.subscriptions.push(flamapy_estimated_number_of_products)
+    context.subscriptions.push(flamapy_leaf_features)
+    context.subscriptions.push(flamapy_max_depth)
+    context.subscriptions.push(flamapy_core_features)
+    context.subscriptions.push(flamapy_dead_features)
+    context.subscriptions.push(flamapy_error_detection)
+    context.subscriptions.push(flamapy_false_optional_features)
+    context.subscriptions.push(flamapy_products_number)
 }
 
 function deactivate() {}    
